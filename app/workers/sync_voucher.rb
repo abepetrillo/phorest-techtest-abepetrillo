d5485ded410e25083b729a668ac77a690be47dd2
Abe Petrillo <abe.petrillo@gmail.com>
2020-09-28 13:15:20 +0100

    Add spec for voucher worker

--- app/workers/sync_voucher.rb
+++ app/workers/sync_voucher.rb
@@ -5,12 +5,15 @@ class SyncVoucher
   # When given the id of a voucher in the database, the worker will
   # attempt to sync the voucher with the phorest API
   def perform(voucher_id)
-    voucher = Voucher.find(voucher_id)
-
-    result = PhorestGatewayService.new.create_voucher!(voucher)
-    voucher.udpate_attributes!(
-      voucher_id: result[:voucherId],
-      synced_at: Time.curent
-    )
+    voucher = Voucher.find_by_id(voucher_id)
+    if voucher
+      result = PhorestGatewayService.new.create_voucher!(voucher)
+      voucher.update_attributes!(
+        voucher_id: result[:voucherId],
+        synced_at: Time.zone.now
+      )
+    else
+      true
+    end
   end
 end
---
+++ spec/workers/sync_voucher_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe SyncVoucher do
+  describe '#perform' do
+    context 'given a voucher that doesnt exist' do
+
+    end
+
+    context 'given a voucher' do
+      context 'that does not exist' do
+        it 'fails gracefully'do
+          result = described_class.new.perform(99999)
+          expect(result).to eql true
+        end
+      end
+
+      context 'that exists' do
+        context 'and is not synced' do
+           it 'syncs and collects a voucher id' do
+            voucher = Voucher.create!(
+              client_id: 'fake_client_id',
+              creating_branch_id: 'fake_branch_id',
+              original_balance: 1000
+            )
+            result = subject.perform(voucher.id)
+            expect(result).to eql true
+            voucher.reload
+            expect(voucher.voucher_id).to be_present
+            expect(voucher.synced_at).to be_present
+          end
+        end
+      end
+    end
+  end
+end
